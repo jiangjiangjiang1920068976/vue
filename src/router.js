58c854dcb60f4bd6cd3113a98413d5606ce2775e
jiangfengyu <1920068976@qq.com>
2020-02-24 07:42:16 +0800

    my project

---
+++ src/router.js
@@ -0,0 +1,13 @@
+
+//1、导入vue-router包
+import VueRouter from 'vue-router'
+
+
+//3、创建路由对象
+var routerObj = new VueRouter({
+    routes:[
+        
+    ]
+})
+//使用ES6将路由对象暴露出去
+export default routerObj
